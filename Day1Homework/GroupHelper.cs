0a90b1496b6b9a722d4caee749f1c08364dfec30
Alger <alger.chen23@gmail.com>
2017-05-26 10:36:08 +0800

    Add GroupHelper implements

--- Day1Homework/GroupHelper.cs
+++ Day1Homework/GroupHelper.cs
@@ -6,12 +6,40 @@ using System.Threading.Tasks;
 
 namespace Day1Homework
 {
-    public class GroupHelper
+    public class GroupHelper: IGroupHelper
     {
-        public int[] GroupSum<T>(IList<T> data, string property, int length)
+        public IEnumerable<int> GroupSum<T>(IList<T> data, Func<T, int> getValueFn, int length)
         {
-            return null;
+            var values = data.Select(getValueFn);
+            return GetGroup(values, length);
         }
+
+        public IEnumerable<int> GroupSum<T>(IList<T> data, string property, int length)
+        {
+            var t = typeof(T);
+            var p = t.GetProperty(property);
+            if (p == null) throw new MissingFieldException(property);
+
+            var values = data.Select(o => (int)Convert.ChangeType(p.GetValue(o), TypeCode.Int32));
+            return GetGroup(values, length);
+        }
+
+        public IEnumerable<int> GetGroup(IEnumerable<int> values, int length)
+        {
+            int index = 0;
+            int total = values.Count();
+            while (index < total)
+            {
+                yield return values.Skip(index).Take(length).Sum();
+                index += length;
+            }
+        }
+
+    }
+    public interface IGroupHelper
+    {
+        IEnumerable<int> GroupSum<T>(IList<T> data, string property, int length);
+        IEnumerable<int> GroupSum<T>(IList<T> data, Func<T,int> getValueFn, int length);
     }
 
 
--- Day1HomeworkTests/GroupSumTests.cs
+++ Day1HomeworkTests/GroupSumTests.cs
@@ -1,19 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Day1Homework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Day1Homework;
 using ExpectedObjects;
-using FluentAssertions;
-using FluentAssertions.Common;
-using NSubstitute;
-using NSubstitute.ExceptionExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Day1Homework.Tests
+namespace Day1HomeworkTests
 {
-    [TestClass()]
+    [TestClass]
     public class GroupSumTests
     {
         private static List<Product> _data;
@@ -43,13 +37,14 @@ namespace Day1Homework.Tests
             // Arrange
             var property = "Cost";
             var length = 3;
-            var expected = new [] {6,15,24,21 };
+            var expected = new[] { 6, 15, 24, 21 };
 
-            var target = Substitute.For<IGroupHelper>();
-            target.GroupSum(_data, property, length).Returns(new[] {6, 15, 24, 21});
+            var target = new GroupHelper();
+            //var target = Substitute.For<IGroupHelper>();
+            //target.GroupSum(_data, property, length).Returns(new[] {6, 15, 24, 21});
 
             // Act
-            var actual = target.GroupSum(_data, property, length);
+            var actual = target.GroupSum(_data, property, length).ToArray();
 
             // Assert
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -63,100 +58,55 @@ namespace Day1Homework.Tests
             var length = 4;
             var expected = new[] { 50, 66, 60 };
 
-            var target = Substitute.For<IGroupHelper>();
-            target.GroupSum(_data, property, length).Returns(new[] { 50, 66, 60 });
+            IGroupHelper target = new GroupHelper();
+            //var target = Substitute.For<IGroupHelper>();
+            //target.GroupSum(_data, property, length).Returns(new[] { 50, 66, 60 });
 
             // Act
-            var actual = target.GroupSum(_data, property, length);
+            var actual = target.GroupSum(_data, property, length).ToArray();
 
             // Assert
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
         [TestMethod]
-        public void GroupSum_Property_NotFound_Should_Throw_Exception()
+        public void GroupSum_By_CostFunc_3_Items_Should_Return_Correct_Summary_Result()
         {
             // Arrange
-            var property = "name_not_exist";
+            Func<Product, int> getCostFunc = o => (int) o.Cost;
             var length = 3;
+            var expected = new[] { 6, 15, 24, 21 };
 
-            var target = Substitute.For<IGroupHelper>();
-            target.GroupSum(_data, Arg.Is<string>(x => typeof(Product).GetProperties().All(p => p.Name != property)), length)
-                .Throws(new ArgumentException(nameof(property)));
+            var target = new GroupHelper();
+            //var target = Substitute.For<IGroupHelper>();
+            //target.GroupSum(_data, property, length).Returns(new[] {6, 15, 24, 21});
 
             // Act
-            Action actual = () => target.GroupSum(_data, property, length);
+            var actual = target.GroupSum(_data, getCostFunc, length).ToArray();
 
             // Assert
-            actual.ShouldThrow<ArgumentException>();
+            expected.ToExpectedObject().ShouldEqual(actual);
         }
 
         [TestMethod]
-        public void GroupSum_With_String_PropertyType_Should_Throw_UnsupportedTypeException()
+        public void GroupSum_By_RevenueFunc_4_Items_Should_Return_Correct_Summary_Result()
         {
             // Arrange
-            var data = new List<Ball>
-            {
-                new Ball() {Id = 1, Cost = "10"},
-                new Ball() {Id = 2, Cost = "20"},
-            };
-            var property = "Cost";
-            var length = 3;
+            Func<Product, int> getRevenue = o => (int)o.Revenue;
+            var length = 4;
+            var expected = new[] { 50, 66, 60 };
 
-            var target = Substitute.For<IGroupHelper>();
-            target.GroupSum(data, Arg.Is<string>(x => typeof(Ball).GetProperty(property).PropertyType == typeof(string)), length)
-                .Throws(new UnsupportedTypeException(nameof(property)));
+            IGroupHelper target = new GroupHelper();
+            //var target = Substitute.For<IGroupHelper>();
+            //target.GroupSum(_data, property, length).Returns(new[] { 50, 66, 60 });
 
             // Act
-            Action actual = () => target.GroupSum(data, property, length);
+            var actual = target.GroupSum(_data, getRevenue, length).ToArray();
 
             // Assert
-            actual.ShouldThrow<UnsupportedTypeException>();
+            expected.ToExpectedObject().ShouldEqual(actual);
         }
 
-        [TestMethod]
-        public void GroupSum_With_Null_Data_Should_Throuw_NullArgumentException()
-        {
-            // Arrange
-            List<Ball> data = null;
-            var property = "Cost";
-            var length = 3;
 
-            var target = Substitute.For<IGroupHelper>();
-            target.GroupSum(data, Arg.Is<string>(x => typeof(Ball).GetProperty(property).PropertyType == typeof(string)), length)
-                .Throws(new ArgumentNullException("Data"));
-
-            // Act
-            Action actual = () => target.GroupSum(data, property, length);
-
-            // Assert
-            actual.ShouldThrow<ArgumentNullException>();
-        }
-    }
-
-
-    public interface IGroupHelper
-    {
-        int[] GroupSum<T>(IList<T> data, string property, int length);
-    }
-
-    public class Product
-    {
-        public int Id { get; set; }
-        public decimal Cost { get; set; }
-        public decimal Revenue { get; set; }
-        public decimal SellPrice { get; set; }
-    }
-    public class Ball
-    {
-        public int Id { get; set; }
-        public string Cost { get; set; }
-    }
-
-    public class UnsupportedTypeException : Exception
-    {
-        public UnsupportedTypeException(){}
-        public UnsupportedTypeException(string message) : base(message) { }
-        public UnsupportedTypeException(string message, Exception innerException) :base(message, innerException){ }
     }
 }
